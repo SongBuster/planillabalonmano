f4978584ff7f8619589d7f678eb7d6262e439285
SongBuster <74537986+SongBuster@users.noreply.github.com>
2024-09-18 17:50:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,33 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.title("🤾🏻 Estadisticas de la planilla de Balonmano 🤾🏻‍♀️")
+
+# URL del archivo en GitHub (reemplaza por tu URL)
+url = "https://raw.githubusercontent.com/SongBuster/planillabalonmano/main/tabla.txt"
+
+
+# Leer el archivo
+@st.cache_data
+def load_data(url):
+    return pd.read_csv(url, sep='\t')
+
+# Cargar los datos
+data = load_data(url)
+
+# Mostrar los primeros datos
+st.subheader('Primeras filas del archivo')
+st.write(data.head())
+
+# Mostrar un resumen estadístico
+st.subheader('Resumen estadístico del archivo')
+st.write(data.describe())
+
+# Generar un gráfico de barras de una columna
+st.subheader('Gráfico de barras')
+column_to_plot = st.selectbox('Selecciona una columna para el gráfico de barras', data.columns)
+
+#fig, ax = plt.subplots()
+#sns.barplot(x=data.index, y=data[column_to_plot], ax=ax)
+#st.pyplot(fig)
